2fca667b593439b2159b352518f3cf52cca6e736
Pavel Švehla <pavel.svehla@gmail.com>
2018-01-28 15:52:10 +0100

    Empty Cms module added for further development

---
+++ Cms/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace Cms
+{
+    public class Startup : StartupBase
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            app.Map("/cms", branch => 
+                branch.Run(context => context.Response.WriteAsync("Hello from CMS module"))
+            );
+        }
+    }
+}
